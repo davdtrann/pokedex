029b4187a3c5b29d6bccb742d9c83789e982f839
David <dovidtran@gmail.com>
2025-11-25 00:50:03 -0500

    Added main.js

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
+const API = require("https://pokeapi.co/api/v2");
+
+function showMenu() {
+    console.log('\n1. Pokenmon')
+    console.log('\n2. Item')
+    console.log('\n3. Move')
+}
+
+function run() {
+    showMenu();
+    
+}
